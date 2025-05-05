aff05885e630da5b521a5de529d140c4d493479d
ProfH403 <ProfH403@lab.aca>
2025-05-05 11:26:07 -0500

    configurando dependencas de bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import MainPage from './pages/MainPage'
+
+import bootstrap from 'bootstrap'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <MainPage />
   </StrictMode>,
 )

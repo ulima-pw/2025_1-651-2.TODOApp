c2ccef7ab0bab5cad724bcb4394dbb7d32d14ec1
ProfH403 <ProfH403@lab.aca>
2025-06-30 12:03:27 -0500

    Pantalla de login implementada

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,42 +1,79 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+const URL = "http://localhost:5000"
 
 const LoginPage = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
 
-  // Define (do not implement) the function to make the HTTP login request
-  const loginRequest = async (username: string, password: string) => {
-    // TODO: Implement HTTP request to backend for login
-  }
+    const navigate = useNavigate()
 
-  return (
-    <div className="container mt-5">
-      <h2>Login</h2>
-      <div className="mb-3">
-        <label htmlFor="username" className="form-label">Username</label>
-        <input
-          type="text"
-          className="form-control"
-          id="username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="password" className="form-label">Password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <button className="btn btn-primary" type="button" onClick={() => loginRequest(username, password)}>
-        Login
-      </button>
-    </div>
-  )
+    // Define (do not implement) the function to make the HTTP login request
+    const loginRequest = async (username: string, password: string) => {
+        const resp = await fetch(`${URL}/usuarios/login`, {
+            method : "POST",
+            headers : {
+                "content-type" : "application/json"
+            },
+            body : JSON.stringify({
+                username : username,
+                password : password
+            })
+        })
+        
+        const data = await resp.json()
+        if (data.msg == "") {
+            // Login correcto
+            sessionStorage.setItem("USUARIO", JSON.stringify(data.usuario))
+            navigate("/main")
+        }else {
+            // Login incorrecto
+            setError(data.msg)
+        }
+    }
+
+    return (
+        <div className="container mt-5">
+            <h2>Login</h2>
+            <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+            </div>
+            {
+                (() => {
+                    if (error != "") {
+                        return <div className="alert alert-danger">
+                            { error }
+                        </div>
+                    }
+                })()
+            }
+            
+            <button className="btn btn-primary"
+                type="button"
+                onClick={() => loginRequest(username, password)}>
+                Login
+            </button>
+        </div>
+    )
 }
 
 export default LoginPage 
